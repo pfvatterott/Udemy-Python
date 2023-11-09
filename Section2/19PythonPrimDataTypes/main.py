8332cdd8af5d1f8694ef6b4cb961da8a8eedc9f2
Paul Vatterott <pfvatterott@gmail.com>
2023-11-08 19:32:26 -0700

    day2

---
+++ Section2/19PythonPrimDataTypes/main.py
@@ -0,0 +1,19 @@
+# String
+
+word = "Hello"
+print(word[4])
+
+# Integer
+
+print(123 + 456)
+
+print(123_456_789)
+
+# Float
+
+print(3.1234)
+
+# Boolean
+
+print(True)
+print(False)
---
+++ Section2/22MathOperations/main.py
@@ -0,0 +1,16 @@
+3 + 5
+7 - 4
+3 * 2
+print(6 / 3)
+# prints 2.0
+
+print(4 ** 2)
+# 4 to the power of 2
+
+print(3 * 3 + 3 / 3 - 3)
+# PEMDAS
+# ()
+# **
+# * /
+# + -
+# prints 7.0
